d58bac77caf22eac06df963986d82c3f08d0c0bf
LanaDelSlay <42992759+LanaDelSlay@users.noreply.github.com>
2021-06-10 19:13:37 -0700

    Suggestive Save Text, automatic loading of IPs and Macs.
    All data is stored in one single file now, and the information is automatically pulled and saved to that location!

--- src/main/java/wakeOnLAN/coolController.java
+++ src/main/java/wakeOnLAN/coolController.java
@@ -1,28 +1,26 @@
 package wakeOnLAN;
 
-import java.awt.Dimension;
-import java.awt.FileDialog;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
+import java.net.URI;
 import java.net.UnknownHostException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
-import javax.swing.JFileChooser;
-import javax.swing.JFrame;
-
-import javafx.application.Platform;
+import impl.org.controlsfx.autocompletion.AutoCompletionTextFieldBinding;
+import impl.org.controlsfx.autocompletion.SuggestionProvider;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -31,212 +29,241 @@ import javafx.scene.control.TextField;
 import javafx.scene.paint.Color;
 
 public class coolController {
-	
-    @FXML
-    private Button loadBtn;
 
-    @FXML
-    private Button wakeBtn;
+	@FXML
+	private Button loadBtn;
 
-    @FXML
-    private Button saveBtn;
-    
-    @FXML
-    private TextField ipTextF;
+	@FXML
+	private Button wakeBtn;
 
-    @FXML
-    private TextField macTextF;
+	@FXML
+	private Button saveBtn;
 
-    @FXML
-    private Label feedbackLabel;
+	@FXML
+	private TextField ipTextF;
 
-    @FXML
-    private TextField compNameTextF;
+	@FXML
+	private TextField macTextF;
 
-    @FXML
-    private Label loadedComputerLabel;
-    
-    @FXML
-    void load(ActionEvent event) throws Exception {
-    	compNameTextF.setVisible(false);
-		feedbackLabel.setVisible(false);
-        loadedComputerLabel.setText("");
-        JFileChooser f = new JFileChooser();
-        String dataFolder = System.getProperty("user.home") + File.separator + "WOL_data";
-        File file = new File(dataFolder);
-        String OS = System.getProperty("os.name");
-        
-        if (OS.contains("Windows")) {
-        	  f.setCurrentDirectory(file);
-              f.setFileSelectionMode(JFileChooser.FILES_ONLY); 
-              f.showSaveDialog(null);
-              
-              try {
-      			FileInputStream fileIn = new FileInputStream(f.getSelectedFile());
-      			ObjectInputStream objectIn = new ObjectInputStream(fileIn);
-      			Object obj = objectIn.readObject();
-      			lanConnection lc = (lanConnection) obj;
-      			ipTextF.setText(lc.getIP());
-      			macTextF.setText(lc.getMac());
-      			loadedComputerLabel.setText(lc.getNickname() + " selected.");
-      			feedbackLabel.setText("Loaded!");
-      			
-      		} catch (FileNotFoundException e) {
-      			feedbackLabel.setText("ERROR: File not found");
-      			e.printStackTrace();
-      		} catch (IOException e) {
-      			// TODO Auto-generated catch block
-      			feedbackLabel.setText("ERROR: File may be corrupt.");
-      			e.printStackTrace();
-      		} catch (ClassNotFoundException e) {
-      			// TODO Auto-generated catch block
-      			feedbackLabel.setText("ERROR");
-      			e.printStackTrace();
-      		} catch (NullPointerException e) {
-      			feedbackLabel.setText("No File selected");
-      		}
-        } else {
-        	macFileDialog t = new macFileDialog(ipTextF, macTextF, feedbackLabel,loadedComputerLabel);
-        	t.start();
-        	
-        }
-        
-    }
+	@FXML
+	private Label feedbackLabel;
 
-    @FXML
-    void save(ActionEvent event) throws IOException {
-    	String name;
+	@FXML
+	private TextField compNameTextF;
+
+	@FXML
+	private Label loadedComputerLabel;
+
+	String userDir = System.getProperty("user.home");
+	String dataFolder = userDir + File.separator + "WOL_data";
+	ArrayList<String> ipStringArr = new ArrayList<String>();
+	ArrayList<String> macStringArr = new ArrayList<String>();
+	ArrayList<String> nameStringArr = new ArrayList<String>();
+
+	public static void main(String args[]) {
+	}
+
+	public void initialize() throws IOException {
+		populateSuggestions();
+
+		ipTextF.textProperty().addListener((obs, oldText, newText) -> {
+			if (ipStringArr.contains(newText)) {
+				int index = ipStringArr.indexOf(newText);
+				String nameStr = nameStringArr.get(index).trim();
+				String nameCapped = nameStr.substring(0, 1).toUpperCase() + nameStr.substring(1);
+				macTextF.setText(macStringArr.get(index));
+				feedbackLabel.setText(nameCapped + " loaded!");
+				feedbackLabel.setTextFill(Color.GREEN);
+				feedbackLabel.setVisible(true);
+			}
+		});
+
+		macTextF.textProperty().addListener((obs, oldText, newText) -> {
+			if (macStringArr.contains(newText)) {
+				int index = macStringArr.indexOf(newText);
+				ipTextF.setText(ipStringArr.get(index));
+				String nameStr = nameStringArr.get(index).trim();
+				String nameCapped = nameStr.substring(0, 1).toUpperCase() + nameStr.substring(1);
+				feedbackLabel.setText(nameCapped.toUpperCase() + " loaded!");
+				feedbackLabel.setTextFill(Color.GREEN);
+				feedbackLabel.setVisible(true);
+			}
+		});
+	}
+
+	@SuppressWarnings("unchecked")
+	@FXML
+	void populateSuggestions() throws IOException {
+		String pattern = "[^,]+";
+		File dataFile = new File(dataFolder + File.separator + "savedIPs.txt");
+		URI dataURI = dataFile.toURI();
+		FileInputStream fileIn;
+		List<String> result = Files.readAllLines(Paths.get(dataURI));
+		Pattern p = Pattern.compile(pattern);
+
+		result.forEach(text -> {
+			Matcher m = p.matcher(text);
+			int i = 1;
+			while (m.find()) {
+				// Format is IP, MAC, then name!
+				switch (i) {
+				case 1:
+					ipStringArr.add(m.group());
+					break;
+				case 2:
+					macStringArr.add(m.group());
+					break;
+				case 3:
+					nameStringArr.add(m.group());
+					break;
+				}
+
+				i++;
+			}
+
+		});
+
+		try {
+			@SuppressWarnings("rawtypes")
+			SuggestionProvider ipSuggestionProvider = SuggestionProvider.create(new ArrayList());
+			new AutoCompletionTextFieldBinding<>(ipTextF, ipSuggestionProvider);
+			ipSuggestionProvider.addPossibleSuggestions("192.168.0.");
+			ipSuggestionProvider.addPossibleSuggestions("10.0.0.");
+			ipSuggestionProvider.addPossibleSuggestions(ipStringArr);
+
+			@SuppressWarnings("rawtypes")
+			SuggestionProvider macSuggestionProvider = SuggestionProvider.create(new ArrayList());
+			new AutoCompletionTextFieldBinding<>(macTextF, macSuggestionProvider);
+			macSuggestionProvider.addPossibleSuggestions("BD-C9-E7-D4-FF-04");
+			macSuggestionProvider.addPossibleSuggestions(macStringArr);
+
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		ipTextF.setText("");
+		macTextF.setText("");
+		// feedbackLabel.setText("Loaded!");
+	}
+
+	@FXML
+	void save(ActionEvent event) throws IOException {
+		String name;
 		feedbackLabel.setTextFill(Color.GREEN);
-    	String ip = ipTextF.getText(); 
-    	String mac = macTextF.getText();
-    	String userDir = System.getProperty("user.home");
-    	String dataFolder = userDir + File.separator + "WOL_data";
-    	Files.createDirectories(Paths.get(dataFolder));
-    		if(verifyInput(ip, mac, feedbackLabel)) {
-                
-                if(compNameTextF.getText().isBlank()||!compNameTextF.isVisible()) {
-                	compNameTextF.setVisible(true);
-                    loadedComputerLabel.setText("Please enter a name");    
-                } else {
-                	FileOutputStream fileOut = new FileOutputStream(dataFolder + File.separator + compNameTextF.getText());
-                    ObjectOutputStream objectOut = new ObjectOutputStream(fileOut);
-                name = compNameTextF.getText();
-                lanConnection lc = new lanConnection(ip,mac,name);
-                objectOut.writeObject(lc);
-                objectOut.close();
-                feedbackLabel.setVisible(true);
-                feedbackLabel.setTextFill(Color.GREEN);
-                feedbackLabel.setText("Saved!");
-                compNameTextF.setVisible(false);
-                loadedComputerLabel.setText("");
-                }
-    		}
-        
-    }
+		String ip = ipTextF.getText();
+		String mac = macTextF.getText();
+		Files.createDirectories(Paths.get(dataFolder));
+		if (verifyInput(ip, mac, feedbackLabel)) {
+			if (compNameTextF.getText().isBlank() || !compNameTextF.isVisible()) {
+				compNameTextF.setVisible(true);
+				loadedComputerLabel.setText("Please enter a name");
+			} else {
+				name = compNameTextF.getText();
+				try (Writer writer = new BufferedWriter(new OutputStreamWriter(
+						new FileOutputStream(dataFolder + File.separator + "savedIPs.txt", true), "utf-8"))) {
+					writer.append("\n" + ip + "," + mac + "," + name);
+				}
+				feedbackLabel.setVisible(true);
+				feedbackLabel.setTextFill(Color.GREEN);
+				feedbackLabel.setText("Saved!");
+				compNameTextF.setVisible(false);
+				loadedComputerLabel.setText("");
+			}
+		}
+	}
 
-    @FXML
-    void wake(ActionEvent event) {
-    	compNameTextF.setVisible(false);
-        loadedComputerLabel.setText("");
+	@FXML
+	void wake(ActionEvent event) {
+		compNameTextF.setVisible(false);
+		loadedComputerLabel.setText("");
 		feedbackLabel.setTextFill(Color.RED);
 		feedbackLabel.setVisible(false);
-    	String ip = ipTextF.getText(); 
-    	String mac = macTextF.getText();
+		String ip = ipTextF.getText();
+		String mac = macTextF.getText();
 
-    	
-    	if(verifyInput(ip,mac,feedbackLabel)) 
-        	try {
-                byte[] macBytes = getMacBytes(mac);
-                byte[] bytes = new byte[6 + 16 * macBytes.length];
-                for (int i = 0; i < 6; i++) {
-                    bytes[i] = (byte) 0xff;
-                }
-                for (int i = 6; i < bytes.length; i += macBytes.length) {
-                    System.arraycopy(macBytes, 0, bytes, i, macBytes.length);
-                }
-                
-                InetAddress address = InetAddress.getByName(ip);
-                DatagramPacket packet = new DatagramPacket(bytes, bytes.length, address, 9);
-                DatagramSocket socket = new DatagramSocket();
-                socket.send(packet);
-                socket.close();
-        		feedbackLabel.setTextFill(Color.GREEN);
-                feedbackLabel.setText("Wake-on-LAN packet sent.");
-        		feedbackLabel.setVisible(true);
-            }
-            catch (UnknownHostException e) {
-        		feedbackLabel.setTextFill(Color.RED);
-            	feedbackLabel.setText("Failed to send Wake-on-LAN packet:");
-        		feedbackLabel.setVisible(true);
-                System.out.println(e);
-                System.exit(1);
-            } catch (SocketException e) {
-            	feedbackLabel.setTextFill(Color.RED);
-            	feedbackLabel.setText("Failed to send Wake-on-LAN packet:");
-        		feedbackLabel.setVisible(true);
-            	e.printStackTrace();
+		if (verifyInput(ip, mac, feedbackLabel))
+			try {
+				byte[] macBytes = getMacBytes(mac);
+				byte[] bytes = new byte[6 + 16 * macBytes.length];
+				for (int i = 0; i < 6; i++) {
+					bytes[i] = (byte) 0xff;
+				}
+				for (int i = 6; i < bytes.length; i += macBytes.length) {
+					System.arraycopy(macBytes, 0, bytes, i, macBytes.length);
+				}
+				InetAddress address = InetAddress.getByName(ip);
+				DatagramPacket packet = new DatagramPacket(bytes, bytes.length, address, 9);
+				DatagramSocket socket = new DatagramSocket();
+				socket.send(packet);
+				socket.close();
+				feedbackLabel.setTextFill(Color.GREEN);
+				feedbackLabel.setText("Wake-on-LAN packet sent.");
+				feedbackLabel.setVisible(true);
+			} catch (UnknownHostException e) {
+				feedbackLabel.setTextFill(Color.RED);
+				feedbackLabel.setText("Failed to send Wake-on-LAN packet:");
+				feedbackLabel.setVisible(true);
+				System.out.println(e);
+				System.exit(1);
+			} catch (SocketException e) {
+				feedbackLabel.setTextFill(Color.RED);
+				feedbackLabel.setText("Failed to send Wake-on-LAN packet:");
+				feedbackLabel.setVisible(true);
+				e.printStackTrace();
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-        	
+	}
 
-    	}
-    	
-    private static byte[] getMacBytes(String macStr) throws IllegalArgumentException {
-        byte[] bytes = new byte[6];
-        String[] hex = macStr.split("(\\:|\\-)");
-        if (hex.length != 6) {
-            throw new IllegalArgumentException("Invalid MAC address.");
-        }
-        try {
-            for (int i = 0; i < 6; i++) {
-                bytes[i] = (byte) Integer.parseInt(hex[i], 16);
-            }
-        }
-        catch (NumberFormatException e) {
-            throw new IllegalArgumentException("Invalid hex digit in MAC address.");
-        }
-        return bytes;
-    }
-    
-    private static Boolean verifyInput(String ip, String mac, Label feedbackLabel) {    	
+	private static byte[] getMacBytes(String macStr) throws IllegalArgumentException {
+		byte[] bytes = new byte[6];
+		String[] hex = macStr.split("(\\:|\\-)");
+		if (hex.length != 6) {
+			throw new IllegalArgumentException("Invalid MAC address.");
+		}
+		try {
+			for (int i = 0; i < 6; i++) {
+				bytes[i] = (byte) Integer.parseInt(hex[i], 16);
+			}
+		} catch (NumberFormatException e) {
+			throw new IllegalArgumentException("Invalid hex digit in MAC address.");
+		}
+		return bytes;
+	}
+
+	private static Boolean verifyInput(String ip, String mac, Label feedbackLabel) {
 		feedbackLabel.setTextFill(Color.RED);
-    	feedbackLabel.setVisible(false);
-    	String ipReg = "^(?:[0-9]{1,3}\\.){3}[0-9]{1,3}$"; 
-    	String macReg = "^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})|([0-9a-fA-F]{4}\\\\.[0-9a-fA-F]{4}\\\\.[0-9a-fA-F]{4})$";
-    	Pattern ipPattern = Pattern.compile(ipReg);
-    	Pattern macPattern = Pattern.compile(macReg);
-    	Matcher ipMatch = ipPattern.matcher(ip);
-    	Matcher macMatch = macPattern.matcher(mac);
-    	
-    	//Is IP Field Empty
-    	if (ip.isBlank()) {
-    		feedbackLabel.setVisible(true);
-    		feedbackLabel.setText("Get fucked nerd");
-    		return false;
-    	}  else
-    	//Is it a valid IP?
-    	if (!ipMatch.matches()) {
-    		feedbackLabel.setVisible(true);
-    		feedbackLabel.setText("IP not valid!");
-    		return false;
-
-    	} else
-    	//Is MAC Blank
-    	if (mac.isBlank()) {
-    		feedbackLabel.setVisible(true);
-    		feedbackLabel.setText("Mac is blank!");
-    		return false;
-    	} else
-    		//Is mac valid?
-    	if (!macMatch.matches()) {
-    		feedbackLabel.setVisible(true);
-    		feedbackLabel.setText("Mac is invalid!");
-    		return false;
-    	}  else {
-    		return true;
-    		
-    }
-}
+		feedbackLabel.setVisible(false);
+		String ipReg = "^(?:[0-9]{1,3}\\.){3}[0-9]{1,3}$";
+		String macReg = "^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})|([0-9a-fA-F]{4}\\\\.[0-9a-fA-F]{4}\\\\.[0-9a-fA-F]{4})$";
+		Pattern ipPattern = Pattern.compile(ipReg);
+		Pattern macPattern = Pattern.compile(macReg);
+		Matcher ipMatch = ipPattern.matcher(ip);
+		Matcher macMatch = macPattern.matcher(mac);
+		// Is IP Field Empty
+		if (ip.isBlank()) {
+			feedbackLabel.setVisible(true);
+			return false;
+		} else
+		// Is it a valid IP?
+		if (!ipMatch.matches()) {
+			feedbackLabel.setVisible(true);
+			feedbackLabel.setText("IP not valid!");
+			return false;
+		} else
+		// Is MAC Blank
+		if (mac.isBlank()) {
+			feedbackLabel.setVisible(true);
+			feedbackLabel.setText("Mac is blank!");
+			return false;
+		} else
+		// Is mac valid?
+		if (!macMatch.matches()) {
+			feedbackLabel.setVisible(true);
+			feedbackLabel.setText("Mac is invalid!");
+			return false;
+		} else {
+			return true;
+		}
+	}
 }
